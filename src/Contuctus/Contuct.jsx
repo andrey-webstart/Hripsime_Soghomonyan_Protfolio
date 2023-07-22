bf8266d41dac661a98e136a9dd00d58bc893dd80
Hripsimes1 <88665664+andrey-webstart@users.noreply.github.com>
2023-07-22 22:15:55 +0400

    new

--- src/Contuctus/Contuct.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Contuctus() {
-  return (
-    <div>
-      Contuctus
-    </div>
-  )
-}
--- src/Photosandphotostory/Photosandphotostory.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import Kondphotosslide from './HripsimeKondFInal/Kondphotosslide/Kondphotosslide'
-
-export default function Photosandphotostory() {
-  return (
-    <Kondphotosslide />
-  )
-}
